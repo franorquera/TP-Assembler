41ca7f44190edecee5f2f675772a86f484b454b5
Fran Orquera <franorquera@Frans-MacBook-Pro.local>
2021-11-17 16:01:04 -0300

    imprimir por pantalla

--- tp_105554.asm
+++ tp_105554.asm
@@ -2,3 +2,37 @@
 ; Trabajo Numero 1 Organizacion del Computador                               ;
 ; Francisco Orquera Lorda, 105554                                            ;  
 ;*****************************************************************************
+
+global	main
+extern  puts
+extern  printf
+extern	gets
+extern  fopen
+extern  fclose
+extern  fgets
+extern  fputs
+extern  sscanf
+
+section .data
+    msjIngresarOperando db "Ingrese un operando inicial por teclado (max 99 caracteres)",0
+    msjOperandoIngresado db	"Usted ingreso %s: ",10,0
+
+section .bss
+    operandoInicial resb 100
+
+main:
+    ; Pido por consola el ingreso del operando incial
+    mov rcx, msjIngresarOperando
+    sub	rsp, 32
+	call puts
+	add	rsp, 32
+    mov rcx, operandoInicial
+    sub rsp, 32
+    call gets
+    add rsp, 32
+    mov rcx, msjOperandoIngresado
+    mov rdx, operandoInicial
+    sub	rsp, 32
+	call printf
+	add	rsp, 32
+    ret
